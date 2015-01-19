f265ef6c29021127618afbfaabf17d75fcda1da0
jercobyte <jercobyte@gmail.com>
2015-01-19 01:31:04 +0100

    Añadidos los controles de visualización de la baraja y otros ajustes en el estilo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,5 @@
 var baraja;
-
-$('#nav-prev').on('click', function(event) {
-    baraja.previous();
-});
-
-$('#nav-next').on('click', function(event) {
-    baraja.next();
-});
+var cardStatus = "open";
 
 $('#beginButton').on('click',function(){
     $('#beginDiv').css('opacity','0');
@@ -16,28 +9,55 @@ $('#beginButton').on('click',function(){
         $('#baraja1').show();
         setTimeout(function(){
             baraja = $('#baraja-el').baraja();
-            if ($('#sidebar > ul').is(":visible") === true) {
-                $('#main-content').css({
-                    'margin-left': '0px'
-                });
-                $('#sidebar').css({
-                    'margin-left': '-210px'
-                });
-                $('#sidebar > ul').hide();
-                $("#container").addClass("sidebar-closed");
-             }
-             baraja.fan( {
-                speed : 500,
-                easing : 'ease-out',
-                range : 10,
-                direction : 'right',
-                origin : { x : 50, y : 2000 },
-                center : true
-            } );
-            $('#baraja1').off();
+            fanBaraja();
+            $('#toggleBaraja').css('display','inline-block');
+            $('#toggleBaraja').on('click',toggleBaraja);
+            $('#nextBaraja').css('display','inline-block');
             $('#baraja1').on('click',function(){
-                $('#actions').css('display','inline-block');
+                $('ul.baraja-container').removeClass("baraja-container").addClass("baraja-big-container");
+                $('.hoteldescription').show();
+                $('#nav-prev').css('display','inline-block');
+                $('#nav-next').css('display','inline-block');
+                $('#toggleBaraja').html("Abrir baraja");
+                cardStatus="closed";
+            });
+            $('#nav-prev').on('click', function(event) {
+                baraja.previous();
+            });
+            $('#nav-next').on('click', function(event) {
+                baraja.next();
             });
         },1000);
     },750);
 });
+
+function fanBaraja(){
+    baraja.fan( {
+        speed : 500,
+        easing : 'ease-out',
+        range : 10,
+        direction : 'right',
+        origin : { x : 50, y : 2000 },
+        center : true
+    } );
+}
+
+function toggleBaraja(){
+    if(cardStatus=="closed"){
+        $('ul.baraja-big-container').removeClass("baraja-big-container").addClass("baraja-container");
+        $('.hoteldescription').hide();
+        fanBaraja();
+        $('#nav-prev').hide();
+        $('#nav-next').hide();
+        $('#toggleBaraja').html('Cerrar baraja');
+        cardStatus="open";
+    }else if(cardStatus=="open"){
+        baraja.close();
+        $('ul.baraja-container').removeClass("baraja-container").addClass("baraja-big-container");
+        $('.hoteldescription').show();
+        $('#nav-prev').css('display','inline-block');
+        $('#nav-next').css('display','inline-block');
+        $('#toggleBaraja').html("Abrir baraja");
+        cardStatus="closed";
+    }
+}
--- controllers/hotelscontroller.js
+++ controllers/hotelscontroller.js
@@ -5,7 +5,8 @@ var offers = [
 		stars: 5,
 		location: 'Madrid',
 		price: '90',
-		web: 'www.webhotel.com'
+		web: 'www.webhotel.com',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur molestie et dui vitae porta. Etiam viverra suscipit libero vitae porta. Quisque consequat sapien eu eros porttitor interdum quis sit amet nisl. Cras posuere nisl sed ex consectetur, et porttitor nunc lacinia. Nulla accumsan auctor aliquet. Nunc pharetra pulvinar augue, quis euismod massa tempor feugiat. Quisque libero lacus, iaculis quis ullamcorper sed, dignissim quis lacus. Praesent rhoncus sodales lacinia. Phasellus et mi eget purus lacinia bibendum eget sit amet lorem.'
 	},
 	{
 		name: 'Ripetta Rooms 2',
@@ -13,7 +14,8 @@ var offers = [
 		stars: 5,
 		location: 'Madrid',
 		price: '90',
-		web: 'www.webhotel.com'
+		web: 'www.webhotel.com',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur molestie et dui vitae porta. Etiam viverra suscipit libero vitae porta. Quisque consequat sapien eu eros porttitor interdum quis sit amet nisl. Cras posuere nisl sed ex consectetur, et porttitor nunc lacinia. Nulla accumsan auctor aliquet. Nunc pharetra pulvinar augue, quis euismod massa tempor feugiat. Quisque libero lacus, iaculis quis ullamcorper sed, dignissim quis lacus. Praesent rhoncus sodales lacinia. Phasellus et mi eget purus lacinia bibendum eget sit amet lorem.'
 	},
 	{
 		name: 'Ripetta Rooms 3',
@@ -21,7 +23,8 @@ var offers = [
 		stars: 5,
 		location: 'Madrid',
 		price: '90',
-		web: 'www.webhotel.com'
+		web: 'www.webhotel.com',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur molestie et dui vitae porta. Etiam viverra suscipit libero vitae porta. Quisque consequat sapien eu eros porttitor interdum quis sit amet nisl. Cras posuere nisl sed ex consectetur, et porttitor nunc lacinia. Nulla accumsan auctor aliquet. Nunc pharetra pulvinar augue, quis euismod massa tempor feugiat. Quisque libero lacus, iaculis quis ullamcorper sed, dignissim quis lacus. Praesent rhoncus sodales lacinia. Phasellus et mi eget purus lacinia bibendum eget sit amet lorem.'
 	},
 	{
 		name: 'Ripetta Rooms 4',
@@ -29,7 +32,8 @@ var offers = [
 		stars: 5,
 		location: 'Madrid',
 		price: '90',
-		web: 'www.webhotel.com'
+		web: 'www.webhotel.com',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur molestie et dui vitae porta. Etiam viverra suscipit libero vitae porta. Quisque consequat sapien eu eros porttitor interdum quis sit amet nisl. Cras posuere nisl sed ex consectetur, et porttitor nunc lacinia. Nulla accumsan auctor aliquet. Nunc pharetra pulvinar augue, quis euismod massa tempor feugiat. Quisque libero lacus, iaculis quis ullamcorper sed, dignissim quis lacus. Praesent rhoncus sodales lacinia. Phasellus et mi eget purus lacinia bibendum eget sit amet lorem.'
 	},
 	{
 		name: 'Ripetta Rooms 5',
@@ -37,7 +41,8 @@ var offers = [
 		stars: 5,
 		location: 'Madrid',
 		price: '90',
-		web: 'www.webhotel.com'
+		web: 'www.webhotel.com',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur molestie et dui vitae porta. Etiam viverra suscipit libero vitae porta. Quisque consequat sapien eu eros porttitor interdum quis sit amet nisl. Cras posuere nisl sed ex consectetur, et porttitor nunc lacinia. Nulla accumsan auctor aliquet. Nunc pharetra pulvinar augue, quis euismod massa tempor feugiat. Quisque libero lacus, iaculis quis ullamcorper sed, dignissim quis lacus. Praesent rhoncus sodales lacinia. Phasellus et mi eget purus lacinia bibendum eget sit amet lorem.'
 	},
 	{
 		name: 'Ripetta Rooms 6',
@@ -45,7 +50,8 @@ var offers = [
 		stars: 5,
 		location: 'Madrid',
 		price: '90',
-		web: 'www.webhotel.com'
+		web: 'www.webhotel.com',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur molestie et dui vitae porta. Etiam viverra suscipit libero vitae porta. Quisque consequat sapien eu eros porttitor interdum quis sit amet nisl. Cras posuere nisl sed ex consectetur, et porttitor nunc lacinia. Nulla accumsan auctor aliquet. Nunc pharetra pulvinar augue, quis euismod massa tempor feugiat. Quisque libero lacus, iaculis quis ullamcorper sed, dignissim quis lacus. Praesent rhoncus sodales lacinia. Phasellus et mi eget purus lacinia bibendum eget sit amet lorem.'
 	}
 ];
 
